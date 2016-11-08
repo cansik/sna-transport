aaa6a9c1469c3957c6e6552fa527c09d0a5f0516
Florian Bruggisser <florian@nexpose.ch>
2016-11-08 14:05:29 +0100

    inital commit of the project

---
+++ src/main/java/ch/fhnw/sna/transport/Main.java
@@ -0,0 +1,10 @@
+package ch.fhnw.sna.transport;
+
+/**
+ * Created by cansik on 08.11.16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
